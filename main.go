063c0aaeb2982c0c93386e5c023394f98e3e4987
Bokova Daria <bokovad2@gmail.com>
2026-04-09 22:42:23 +0300

    Add simple Go REST API

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Todo struct {
+	ID   int    `json:"id"`
+	Task string `json:"task"`
+}
+
+var todos []Todo
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(todos)
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	json.NewDecoder(r.Body).Decode(&todo)
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(todo)
+}
+
+func main() {
+	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			getTodos(w, r)
+		} else if r.Method == http.MethodPost {
+			addTodo(w, r)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
