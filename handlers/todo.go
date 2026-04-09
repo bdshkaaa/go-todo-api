73620ea19c3acce384a64845bee9986d38acc736
Bokova Daria <bokovad2@gmail.com>
2026-04-09 23:18:04 +0300

    Add handlers for todos

--- handlers/todo.go
+++ handlers/todo.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"go-todo-api/models"
+	"go-todo-api/storage"
+)
+
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+	todos, _ := storage.LoadTodos()
+	json.NewEncoder(w).Encode(todos)
+}
+
+func AddTodo(w http.ResponseWriter, r *http.Request) {
+	var newTodo models.Todo
+
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	todos, _ := storage.LoadTodos()
+	newTodo.ID = len(todos) + 1
+
+	todos = append(todos, newTodo)
+	storage.SaveTodos(todos)
+
+	json.NewEncoder(w).Encode(newTodo)
+}
